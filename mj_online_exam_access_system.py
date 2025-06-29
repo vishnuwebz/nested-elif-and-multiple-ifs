21bc8b8b5544285f3d34ae97a7f42b197df65f8b
Vishnu G <vishnugkannath@gmail.com>
2025-06-29 19:29:38 +0530

    Added major project bases on online exam access control

---
+++ mj_online_exam_access_system.py
@@ -0,0 +1,13 @@
+# Major Project: Online Exam Access System
+
+registered = True
+face_verified = True
+exam_code_entered = False
+
+if registered and face_verified and exam_code_entered:
+    print("Access granted to exam portal")
+else:
+    print("Access denied. Check verification steps.")
+
+
+# Access denied. Check verification steps.
