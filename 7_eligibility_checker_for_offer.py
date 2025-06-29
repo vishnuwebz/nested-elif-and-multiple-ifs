feef78d4494f4cfd16be632459004eba80d5cd2c
Vishnu G <vishnugkannath@gmail.com>
2025-06-29 19:24:11 +0530

    Added access blocker

---
+++ 7_eligibility_checker_for_offer.py
@@ -0,0 +1,9 @@
+# 2 Eligibility check for offer
+
+is_registered = True
+referral_code = False
+
+if is_registered or referral_code:
+    print("You're eligible for the offer")
+
+# You're eligible for the offer
---
+++ 8_access blocker.py
@@ -0,0 +1,10 @@
+# 3 access blocker
+
+blocked = True
+
+if not blocked:
+    print("Access granted")
+else:
+    print("Access denied")
+
+# Access denied
